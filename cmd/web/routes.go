a925d60e1d8f8b8aeea769e4e90341ba1fddb1cc
Onur DEGERLI <onde@boozt.com>
2024-02-11 22:00:54 +0100

    Add image

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,13 +10,6 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	/*
-		mux := pat.New()
-
-		mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-		mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	*/
-
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -25,6 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
